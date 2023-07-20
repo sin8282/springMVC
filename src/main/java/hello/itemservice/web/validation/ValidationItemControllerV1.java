e2ad2355b5ded78d25177a949b6e056d9bd7235b
sin8282 <sweetejr777@gmail.com>
2023-07-20 15:02:37 +0900

    feat: Validateion v2 init

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -41,28 +44,29 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
-        Map<String, String> errors = new HashMap<>();
-
+    public String addItem(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //BindingResult는 model 뒤(오른쪽)에 선언해야 사용가능하다
         if(!StringUtils.hasText(item.getItemName())){
-            errors.put("itemName", "상품 이름은 필수입니다.");
+            //bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다."));
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
         }
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 100000){
-            errors.put("price", "가격은 1,000 ~ 100,000 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null,"가격은 1,000 ~ 100,000 까지 허용합니다."));
+
         }
         if(item.getQuantity() == null || item.getQuantity() >999){
-            errors.put("quantity", "수량은 1 ~ 999 까지 허용합니다.");
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null,"수량은 1 ~ 999 까지 허용합니다."));
+
         }
 
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
-                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 " + resultPrice);
+                bindingResult.addError(new ObjectError("item",  "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 " + resultPrice));
             }
         }
 
-        if(!errors.isEmpty()){
-            model.addAttribute("errors", errors);
+        if(bindingResult.hasErrors()){
             return "validation/v1/addForm";
         }
         Item savedItem = itemRepository.save(item);
