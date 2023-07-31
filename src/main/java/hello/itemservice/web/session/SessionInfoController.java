764378c428a71847d718db44b66fc7c7d553180d
sin8282 <sweetejr777@gmail.com>
2023-07-31 10:47:51 +0900

    feat: Login Spring Session Info

---
+++ src/main/java/hello/itemservice/web/session/SessionInfoController.java
@@ -0,0 +1,32 @@
+package hello.itemservice.web.session;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+
+@Slf4j
+@RestController
+public class SessionInfoController {
+
+    @GetMapping("/session-info")
+    public String sessionInfo(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        if(session == null){
+            return "세션이 없습니다.";
+        }
+
+        session.getAttributeNames().asIterator()
+                .forEachRemaining(n -> log.info("session name={}, value={}", n, session.getAttribute(n)));
+
+        log.info("sessionId={}", session.getId());
+        log.info("maxInactiveInterval={}", session.getMaxInactiveInterval());
+        log.info("creationTime={}", new Date(session.getCreationTime()));
+        log.info("lastAccessedTime={}", new Date(session.getLastAccessedTime()));
+        log.info("isNew={}", session.isNew());
+        return "세션 출력";
+    }
+}
