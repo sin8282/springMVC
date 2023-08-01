b8361700ed65923edf3b75e5731810364bbb8596
sin8282 <sweetejr777@gmail.com>
2023-08-01 14:29:04 +0900

    feat: Servlet Api Exception start

--- src/main/java/hello/itemservice/web/WebConfig.java
+++ src/main/java/hello/itemservice/web/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(2)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/members/add", "/login", "/logout",
-                        "/css/**", "/*.ico", "/error");
+                        "/css/**", "/*.ico", "/error", "/error-page/**", "/api/**");
     }
 
     //@Bean
