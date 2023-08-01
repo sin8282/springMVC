b83a942f1f05846c577014a73ad9c28e4f963e6a
sin8282 <sweetejr777@gmail.com>
2023-08-01 15:57:12 +0900

    feat: Spring defaultException, ResponseStatus

--- src/main/java/hello/itemservice/web/exception/api/ApiExceptionController.java
+++ src/main/java/hello/itemservice/web/exception/api/ApiExceptionController.java
@@ -1,12 +1,16 @@
 package hello.itemservice.web.exception.api;
 
+import hello.itemservice.web.exception.ex.BadRequestException;
 import hello.itemservice.web.exception.ex.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -25,6 +29,25 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1(){
+        // BadRequestException에 ResponseStatus를 커스터마이징 가능하다.
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2(){
+        //직접 수정이 불가능한 코드에는 (라이브러리 소스같은..) 다음과 같이 ResponseStatusException을 이용한다.
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data){
+        // Spring은 기본적으로 예외처리를 진행해준다. int변수 타입이 missMatching이 일어나면 500 에러가 아니라 400 에러로 바꿔준다.
+        // 이러한 몇가지 예외처리는 DefaultHandlerExceptionResolver 여기서 확인 가능하다.
+        return "ok";
     }
 
     @Data
