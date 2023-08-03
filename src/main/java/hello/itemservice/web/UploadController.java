0c56207e90bf9bd59b098aae60ebad44847d6548
sin8282 <sweetejr777@gmail.com>
2023-08-03 16:34:15 +0900

    feat: FileUpdload, Download System Example

--- src/main/java/hello/itemservice/web/UploadController.java
+++ src/main/java/hello/itemservice/web/UploadController.java
@@ -1,14 +1,25 @@
 package hello.itemservice.web;
 
+import hello.itemservice.domain.multipart.Item;
+import hello.itemservice.domain.multipart.ItemForm;
+import hello.itemservice.domain.multipart.ItemRepository;
+import hello.itemservice.domain.multipart.UploadFile;
+import hello.itemservice.web.multipart.FileStore;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,12 +27,19 @@ import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
+import java.util.List;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class UploadController {
+
+    @javax.annotation.Resource( name = "multipartRepository")
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
 
     @Value("${file.dir}")
     private String fileDir;
@@ -108,4 +126,60 @@ public class UploadController {
         return "multipart/upload-form";
     }
 
+
+    //FileUpload 예제---------------
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form) {
+        return "multipart/item-form";
+    }
+
+    @PostMapping("items/new")
+    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes redirectAttributes) throws IOException {
+
+        //받아올땐 ItemForm으로 multipart로 받아옴
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
+        List<UploadFile> storeImageFiles = fileStore.storeFiles(form.getImageFiles());
+
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setAttachFile(attachFile);
+        item.setImageFiles(storeImageFiles);
+        itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", item.getId());
+
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/items/{id}")
+    public String items(@PathVariable Long id, Model model) {
+        Item item = itemRepository.findById(id);
+        model.addAttribute("item", item);
+        return "multipart/item-view";
+    }
+
+    // 파일 접속 및 다운 권한을 줄 필요가 있음. spring security or custom auth system
+    @ResponseBody
+    @GetMapping("/images/{fileName}")
+    public Resource downloadImage(@PathVariable String fileName) throws MalformedURLException {
+        return new UrlResource("file:" + fileStore.getFullPath(fileName));
+    }
+
+    // 파일 접속 및 다운 권한을 줄 필요가 있음. spring security or custom auth system
+    @GetMapping("/download/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource resource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}", uploadFileName);
+        String encodeUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodeUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(resource);
+    }
 }
