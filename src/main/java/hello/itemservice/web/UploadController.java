9ef9c09e15f322cefb4934cdcae74c30e3f84766
sin8282 <sweetejr777@gmail.com>
2023-08-03 14:23:25 +0900

    feat: Servlet Multipart FileUpload

---
+++ src/main/java/hello/itemservice/web/UploadController.java
@@ -0,0 +1,87 @@
+package hello.itemservice.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+public class UploadController {
+
+    @Value("${file.dir}")
+    private String fileDir;
+
+    @GetMapping("/v1/upload")
+    public String newFile(){
+        return "multipart/upload-form";
+    }
+
+    @PostMapping("/v1/upload")
+    public String saveFileV1(HttpServletRequest request) throws ServletException, IOException {
+        log.info("request={}", request);
+
+        String itemName = request.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        Collection<Part> parts = request.getParts();
+        log.info("parts={}", parts);
+
+        return "multipart/upload-form";
+    }
+
+    @GetMapping("/v2/upload")
+    public String newFileV2(){
+        return "multipart/upload-form";
+    }
+
+    @PostMapping("/v2/upload")
+    public String saveFileV2(HttpServletRequest request) throws ServletException, IOException {
+        log.info("request={}", request);
+
+        String itemName = request.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        Collection<Part> parts = request.getParts();
+        log.info("parts={}", parts);
+
+        for (Part part :
+                parts) {
+            log.info("==== Part ====");
+            log.info("name={}", part.getName());
+            Collection<String> headerNames = part.getHeaderNames();
+            for (String headerName : headerNames) {
+                log.info("header {} : {}", headerName, part.getHeader(headerName));
+            }
+
+           log.info("submitted FileName={}", part.getSubmittedFileName());
+           log.info("size={}", part.getSize());
+
+           //데이터 읽기
+            InputStream is = part.getInputStream();
+            String body = StreamUtils.copyToString(is, StandardCharsets.UTF_8);
+            log.info("body={}", body);
+
+
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+                String fullPath = fileDir + part.getSubmittedFileName();
+                log.info("파일 저장 fullPath={}", fullPath);
+                part.write(fullPath);
+            }
+        }
+
+        return "multipart/upload-form";
+    }
+
+}
